82a7c76edcd609ce7b93b0f9da1e4a67d9431f71
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-14 13:14:19 +0200

    Delete user factory & remove email verified field from user & adjust seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,14 +5,16 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Zeugnis Zauberer',
+            'email' => 'zauberer@bsiag.com',
+            'password' => Hash::make('test1234'),
         ]);
     }
 }
