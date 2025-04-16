e8921fe247e688cd1c93b90b01033c6d628f9615
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-16 15:05:53 +0200

    Add subject resource & add subject type relationship to subject model

---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Filament\Resources\SubjectResource\RelationManagers;
+use App\Models\Subject;
+use App\Models\SubjectType;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Administration';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make("subject_type_id")
+                    ->label("Subject Type")
+                    ->options(SubjectType::all()->pluck("name", "id"))
+                    ->native(false)
+                    ->required(),
+                TextInput::make("name")
+                    ->maxLength(255)
+                    ->unique()
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("subjectType.name")
+                    ->width(0),
+                TextColumn::make("name"),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make()
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubjects::route('/'),
+        ];
+    }
+}
--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subject extends Model
 {
-    //
+    public function subjectType(): BelongsTo {
+        return $this->belongsTo(SubjectType::class);
+    }
 }
