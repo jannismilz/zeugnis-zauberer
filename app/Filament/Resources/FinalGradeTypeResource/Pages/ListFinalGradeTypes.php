8a273ba7ac9eab91a600fc995e19aa5eb0788752
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-16 14:44:28 +0200

    Add final grade type resource

---
+++ app/Filament/Resources/FinalGradeTypeResource/Pages/ListFinalGradeTypes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\AssignmentTypeResource\Pages;
+
+use App\Filament\Resources\AssignmentTypeResource;
+use App\Filament\Resources\FinalGradeTypeResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListFinalGradeTypes extends ListRecords
+{
+    protected static string $resource = FinalGradeTypeResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
