2d09ac64622034434913a54d945b6807d0db86e3
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-16 14:48:27 +0200

    Add subject type resource

---
+++ app/Filament/Resources/SubjectTypeResource.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AssignmentTypeResource\Pages;
+use App\Models\SubjectType;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SubjectTypeResource extends Resource
+{
+    protected static ?string $model = SubjectType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Administration';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->maxLength(255)
+                    ->unique()
+                    ->columnSpanFull()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+               TextColumn::make('name')
+                    ->searchable(),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make()
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubjectTypes::route('/'),
+        ];
+    }
+}
