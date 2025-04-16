e20dda63cd840551c81987313d733371a36246d9
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-16 14:22:26 +0200

    Add all database tables and models

--- database/migrations/0001_01_01_000000_init_tables.php
+++ database/migrations/0001_01_01_000000_init_tables.php
@@ -8,11 +8,15 @@ return new class extends Migration
 {
     public function up(): void
     {
+        /**
+         * User
+         */
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->timestamp("apprentice_start")->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,6 +36,9 @@ return new class extends Migration
             $table->integer('last_activity')->index();
         });
 
+        /**
+         * Cache
+         */
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
@@ -42,6 +49,96 @@ return new class extends Migration
             $table->string('key')->primary();
             $table->string('owner');
             $table->integer('expiration');
+        });
+
+        /**
+         * Assignment
+         */
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("school_class_id");
+            $table->foreignId("subject_id");
+            $table->foreignId("assignment_type_id");
+            $table->string("name");
+            $table->float("weight");
+            $table->timestamps();
+        });
+
+        Schema::create('assignment_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        /**
+         * FinalGrade
+         */
+        Schema::create('final_grades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->foreignId("subject_id");
+            $table->foreignId("final_grade_type_id");
+            $table->float("grade");
+            $table->timestamps();
+        });
+
+        Schema::create('final_grade_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        /**
+         * Grade
+         */
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->foreignId("assignment_id");
+            $table->float("grade");
+            $table->timestamps();
+        });
+
+        /**
+         * SchoolClass
+         */
+        Schema::create('school_classes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("school_class_type_id");
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        Schema::create('school_class_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        /**
+         * UserSchoolClass
+         */
+        Schema::create('user_school_classes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id");
+            $table->foreignId("school_class_id");
+            $table->timestamps();
+        });
+
+        /**
+         * Subject
+         */
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("subject_type_id");
+            $table->string("name");
+            $table->timestamps();
+        });
+
+        Schema::create('subject_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->timestamps();
         });
     }
 
