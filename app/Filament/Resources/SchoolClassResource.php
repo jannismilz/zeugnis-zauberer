e4921ee66f23809a9ae20a3aba749b77ac106265
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-23 10:12:46 +0200

    Add school class resource & add school class type relationship to school class model

---
+++ app/Filament/Resources/SchoolClassResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SchoolClassResource\Pages;
+use App\Filament\Resources\SchoolClassResource\RelationManagers;
+use App\Models\SchoolClass;
+use App\Models\SchoolClassType;
+use App\Models\SubjectType;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SchoolClassResource extends Resource
+{
+    protected static ?string $model = SchoolClass::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Administration';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make("school_class_type_id")
+                    ->label("School Class Type")
+                    ->options(SchoolClassType::all()->pluck("name", "id"))
+                    ->native(false)
+                    ->required(),
+                TextInput::make("name")
+                    ->maxLength(255)
+                    ->unique()
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("schoolClassType.name")
+                    ->width(0),
+                TextColumn::make("name"),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make()
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchoolClasses::route('/'),
+        ];
+    }
+}
--- app/Models/SchoolClass.php
+++ app/Models/SchoolClass.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SchoolClass extends Model
 {
-    //
+    public function schoolClassType(): BelongsTo {
+        return $this->belongsTo(SchoolClassType::class);
+    }
 }
