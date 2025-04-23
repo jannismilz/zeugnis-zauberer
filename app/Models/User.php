01e591c35a06c7d2800bec03f514706703335dc9
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-23 12:40:45 +0200

    Extend user model & database seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Carbon\Traits\Creator;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -20,6 +22,7 @@ class User extends Authenticatable implements FilamentUser
     {
         return [
             'password' => 'hashed',
+            'apprentice_start' => 'datetime'
         ];
     }
 
@@ -27,5 +30,9 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
         return true;
+    }
+
+    public function getApprenticeYearAttribute(): int {
+        return $this->apprentice_start->diffInYears(Carbon::now()) + 1;
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Zeugnis Zauberer',
             'email' => 'zauberer@bsiag.com',
             'password' => Hash::make('test1234'),
+            'apprentice_start' => Carbon::parse("2022-08-01")
         ]);
     }
 }
