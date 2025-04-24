a58a4e35b9f6ea4541af006b80cbc0038d0c1f11
Jannis Milz <jannis.milz@bsi-software.com>
2025-04-24 14:26:38 +0200

    Add logo & change app name & remove dashboard page

--- app/Providers/Filament/RootPanelProvider.php
+++ app/Providers/Filament/RootPanelProvider.php
@@ -40,14 +40,7 @@ class RootPanelProvider extends PanelProvider
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -84,6 +77,8 @@ class RootPanelProvider extends PanelProvider
                     ->customProfileComponents([
                         ExtendedEditProfileForm::class
                     ]),
-            ]);
+            ])
+            ->favicon(asset('logo.png'))
+            ->brandLogo(asset('logo.png'));
     }
 }
